ea83e41f1d08f61c5e9fbeb91f3f078dc7b15513
Shalitha Suranga <shalithasuranga@gmail.com>
2022-06-10 02:17:40 +0530

    Use new Expo notifications API

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Notifications } from 'expo';
-import * as Permissions from 'expo-permissions'
+import * as Notifications from 'expo-notifications';
 
 import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from './FeedNavigator';
@@ -10,26 +9,65 @@ import AccountNavigator from './AccountNavigator';
 import expoPushTokensApi from '../api/expoPushTokens';
 import NewListingButton from './NewListingButton';
 
-
 const Tab = createBottomTabNavigator();
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true
+  }),
+});
+
 const AppNavigator = () => {
-  useEffect(() => {
-    registerForPushNotifications();
-  }, [])
 
+    const notificationListener = useRef();
+    const responseListener = useRef();
 
-  const registerForPushNotifications = async () => { 
-    try {
-       const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-       if (!permission.granted) return;
+    useEffect(() => {
+        // Get a token
+        registerForPushNotificationsAsync()
+            .then(token => expoPushTokensApi.register(token));
 
-       const token = await Notifications.getExpoPushTokenAsync();
-       console.log(token);
-    } catch (error) {
-      console.log('Error getting a token', error);
+        // This listener is fired whenever a notification is received while the app is foregrounded
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            console.log('--- notification received ---');
+            console.log(notification);
+            console.log('------');
+        });
+
+        // This listener is fired whenever a user taps on or interacts with a notification
+        // (works when app is foregrounded, backgrounded, or killed)
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log('--- notification tapped ---');
+            console.log(response);
+            console.log('------');
+        });
+
+        // Unsubscribe from events
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
+
+    async function registerForPushNotificationsAsync() {
+        let token;
+
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+
+        return token;
     }
-  }
 
   return (
     <Tab.Navigator>
