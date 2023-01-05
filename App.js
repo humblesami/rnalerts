176cb6c06771d763499d3086c7110e2e02a550f5
sami92news <sami.akram@92newshd.tv>
2023-01-05 15:50:39 +0500

    version resolved

--- App.js
+++ App.js
@@ -4,10 +4,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 
+
+import { LogBox } from 'react-native';
+// Ignore log notification by message
+LogBox.ignoreLogs(['Warning: ...']);
+//Ignore all log notifications
+LogBox.ignoreAllLogs();
+
+
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer theme={navigationTheme}>
+            <AppNavigator />
+        </NavigationContainer>
+    );
 };
