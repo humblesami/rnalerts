8164665472f23c511db4e9114eda2401ce071be9
sami92news <sami.akram@92newshd.tv>
2023-01-06 15:36:06 +0500

    used expo eject and shifted to react native

--- ignoreWarnings.js
+++ ignoreWarnings.js
@@ -6,6 +6,7 @@ if (__DEV__) {
         // "[fuego-swr-keys-from-collection-path]",
         // "Setting a timer for a long period of time",
         "ViewPropTypes will be removed from React Native",
+        "Stack Navigator",
         // "AsyncStorage has been extracted from react-native",
         // "exported from 'deprecated-react-native-prop-types'.",
         // "Non-serializable values were found in the navigation state.",
