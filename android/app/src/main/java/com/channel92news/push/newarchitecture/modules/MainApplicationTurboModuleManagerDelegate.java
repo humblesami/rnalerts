5190cf2a62727bbaeca680eaa96621d0f2670dec
sami92news <sami.akram@92newshd.tv>
2023-01-27 15:14:36 +0500

    icons and name

--- android/app/src/main/java/com/channel92news/push/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/channel92news/push/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -41,7 +41,7 @@ public class MainApplicationTurboModuleManagerDelegate
     if (!sIsSoLibraryLoaded) {
       // If you change the name of your application .so file in the Android.mk file,
       // make sure you update the name here as well.
-      SoLoader.loadLibrary("donewithit_appmodules");
+      SoLoader.loadLibrary("serveralerts92_appmodules");
       sIsSoLibraryLoaded = true;
     }
   }
